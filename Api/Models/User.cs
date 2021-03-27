c4541caa67ee1e000f39d4659e4f1beaebd94c6d
AdrianMartin <am.dev.8080@gmail.com>
2021-03-27 17:44:21 +0400

    Init

---
+++ Api/Models/User.cs
@@ -0,0 +1,10 @@
+namespace VirtaApi.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+    }
+}
