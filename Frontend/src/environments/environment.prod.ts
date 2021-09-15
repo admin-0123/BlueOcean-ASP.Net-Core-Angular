e28e1f6e4a7ce98ae0e3f99ff0b4fadedaadb4b0
AdrianMartin <am.dev.8080@gmail.com>
2021-09-15 04:04:15 +0400

    prod

--- Frontend/src/environments/environment.prod.ts
+++ Frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://localhost:5001/api/',
-    hubUrl: 'https://localhost:5001/hubs/'
+    apiUrl: 'https://virta.azurewebsites.net/api/',
+    hubUrl: 'https://virta.azurewebsites.net/hubs/'
 };
